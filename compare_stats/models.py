1241d3e39057ff43a0fba503801884d41236d941
caseymm <caseymm@gmail.cpom>
2013-04-10 13:50:47 -0400

    models updated

--- compare_stats/models.py
+++ compare_stats/models.py
@@ -5,24 +5,34 @@ class State(models.Model):
     name = models.CharField(unique=True, max_length=100)
     # biodiversity stats
     birds_in_region = models.CharField(unique=False, max_length=100)
-    protected_land = models.CharField(unique=False, max_length=25)
+    protected_land_image = models.CharField(unique=False, max_length=25)
+    protected_land_percentage = models.CharField(unique=False, max_length=25)
     # economic stats
     revenue = models.IntegerField(unique=False, max_length=25)
     income = models.IntegerField(unique=False, max_length=25)
+    income_text = models.CharField(unique=False, max_length=25)
     industry = models.CharField(unique=False, max_length=100)
     # education stats
     teacher = models.CharField(unique=False, max_length=25)
     student_teacher_ratio = models.CharField(unique=False, max_length=25)
+    student_teacher_text = models.CharField(unique=False, max_length=25)
     teacher_salary = models.IntegerField(unique=False, max_length=25)
     literacy_rate = models.IntegerField(unique=False, max_length=25)
+    literacy_rate_text = models.CharField(unique=False, max_length=25)
     # weather stats
     rainfall = models.IntegerField(unique=False, max_length=25)
+    rainfall_text = models.CharField(unique=False, max_length=25)
     temperature = models.IntegerField(unique=False, max_length=25)
+    temperature_text = models.CharField(unique=False, max_length=25)
     humidity = models.IntegerField(unique=False, max_length=25)
+    humidity_text = models.CharField(unique=False, max_length=25)
     elevation = models.IntegerField(unique=False, max_length=25)
+    elevation_text = models.CharField(unique=False, max_length=25)
     # health stats
-    distance_to_hospital = models.IntegerField(unique=False, max_length=25)
-    percent_of_budget_allocated = models.IntegerField(unique=False, max_length=25)
+    distance_to_hospital = models.CharField(unique=False, max_length=25)
+    distance_to_hospital_text = models.CharField(unique=False, max_length=25)
+    percent_of_budget_allocated = models.CharField(unique=False, max_length=25)
+    percent_of_budget_allocated_text = models.CharField(unique=False, max_length=25)
     
     def __unicode__(self):
         return U'%s %s' %(self.income, self.name)
@@ -36,24 +46,34 @@ class Galapagos(models.Model):
     name = models.CharField(unique=True, max_length=100)
     # biodiversity stats
     birds_in_region = models.CharField(unique=False, max_length=100)
-    protected_land = models.CharField(unique=False, max_length=25)
+    protected_land_image = models.CharField(unique=False, max_length=25)
+    protected_land_percentage = models.CharField(unique=False, max_length=25)
     # economic stats
     revenue = models.IntegerField(unique=False, max_length=25)
     income = models.IntegerField(unique=False, max_length=25)
+    income_text = models.CharField(unique=False, max_length=25)
     industry = models.CharField(unique=False, max_length=100)
     # education stats
     teacher = models.CharField(unique=False, max_length=25)
     student_teacher_ratio = models.CharField(unique=False, max_length=25)
+    student_teacher_text = models.CharField(unique=False, max_length=25)
     teacher_salary = models.IntegerField(unique=False, max_length=25)
     literacy_rate = models.IntegerField(unique=False, max_length=25)
+    literacy_rate_text = models.CharField(unique=False, max_length=25)
     # weather stats
     rainfall = models.IntegerField(unique=False, max_length=25)
+    rainfall_text = models.CharField(unique=False, max_length=25)
     temperature = models.IntegerField(unique=False, max_length=25)
+    temperature_text = models.CharField(unique=False, max_length=25)
     humidity = models.IntegerField(unique=False, max_length=25)
+    humidity_text = models.CharField(unique=False, max_length=25)
     elevation = models.IntegerField(unique=False, max_length=25)
+    elevation_text = models.CharField(unique=False, max_length=25)
     # health stats
-    distance_to_hospital = models.IntegerField(unique=False, max_length=25)
-    percent_of_budget_allocated = models.IntegerField(unique=False, max_length=25)
+    distance_to_hospital = models.CharField(unique=False, max_length=25)
+    distance_to_hospital_text = models.CharField(unique=False, max_length=25)
+    percent_of_budget_allocated = models.CharField(unique=False, max_length=25)
+    percent_of_budget_allocated_text = models.CharField(unique=False, max_length=25)
     
     def __unicode__(self):
         return U'%s %s' %(self.income, self.name)
