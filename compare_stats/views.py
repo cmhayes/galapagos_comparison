4f8ee0590c93846f6afcae1ef7ed532a483dc287
caseymm <caseymm@gmail.cpom>
2013-04-09 20:43:24 -0400

    have information coming up on page

--- compare_stats/views.py
+++ compare_stats/views.py
@@ -2,13 +2,20 @@
 from compare_stats.models import State, Galapagos
 from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 
-def home(request, pk):
-    specificState = get_object_or_404(State, id=pk)
+def home(request):
     states = State.objects.all()
-    galapagos = get_object_or_404(Galapagos, id=pk) 
+    galapagos = Galapagos.objects.all()
     context = {
-        'specificState': specificState,
+        'states': states,
         'galapagos': galapagos,
     }
     
     return render(request, "compare_stats/home.html", context)
+
+def location(request, pk):
+    specificState = get_object_or_404(State, id=pk)
+    specificGalapagos = get_object_or_404(Galapagos, id=pk)
+    context = {
+        'specificState': specificState,
+        'specificGalapagos': specificGalapagos,
+    }
