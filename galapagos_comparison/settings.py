1fb7ee88db8fd7a377c0654c8245fa42a424bae1
steven-king <cujophoto@gmail.com>
2013-05-01 10:41:08 -0400

    chaning static dir to remove appache server

--- galapagos_comparison/settings.py
+++ galapagos_comparison/settings.py
@@ -62,12 +62,13 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static_media')
+STATIC_ROOT = 'http://uncviscom.webfactional.com/static/galapagos_django/static_media/'
+#STATIC_ROOT = os.path.join(PROJECT_PATH, 'static_media')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
+#STATIC_URL = '/static/'
+STATIC_URL = 'http://uncviscom.webfactional.com/static/galapagos_django/static_media/'
 
 
 # Additional locations of static files
@@ -75,7 +76,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'static_media'),
+    'http://uncviscom.webfactional.com/static/galapagos_django/static_media/'
+    #os.path.join(PROJECT_ROOT, 'static_media'),
 )
 
 # List of finder classes that know how to find static files in
